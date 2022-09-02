7f6dc7a2e1803f67856c8e0761ed850f09ada48a
FZZkill <changzhijie11@outlook.com>
2022-09-02 11:43:51 +0800

    choose qython

--- Qython.py
+++ Qython.py
@@ -7,14 +7,59 @@ from source import io
 
 argc = sys.argv
 
-def check() :
-    ...
-print(io.IoUtil.counter(io.IoUtil, argc[1]))
-# if __name__ == "__main__" :
-    # try :
-        # f = open(argc[1])
-        # f.close()
-        # check.foreCheck(argc[1])
-    # except FileNotFoundError:
-        # check()
+version = False
+compiler = False
+about = False
+output = False
+default = False
 
+files = []
+
+def func() :
+    print("""
+            This is Qython Complier
+            Qython to Python, just like tsc of TypeScript
+            ===========
+            default :
+                Can to complier
+
+            --version :
+                print version
+
+            --Complier / -c :
+                Complier file
+                
+                    --output / -o :
+                        set output file name
+                        Note that the operation name is from left to right!
+
+            --about :
+                show this
+
+            --exit:
+                not to do
+            """)
+
+def check_1(argc) :
+    global files, compiler, version, compiler, about, output, default
+    flame = False
+    for i in argc :
+        if i == "--Complier" or i == "-c" :
+            compiler = True
+        elif i == "--version" or i == "-v" :
+            version = True
+        elif i == "--about" :
+            about = True
+        elif i == "--output" or i == "-o" :
+            output = True
+        elif i == "--exit" :
+            sys.exit(0)
+        elif compiler :
+            files.append(i)
+        else :
+            raise Exception("Error in "+ i)
+
+if __name__ == "__main__" :
+    check_1(argc)
+    for i in files :
+        check.foreCheck(i)
